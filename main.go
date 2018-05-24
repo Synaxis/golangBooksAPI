0eadd2adb6ba20ee59ff5adbe60986d6ea6a2ba5
Synaxis <01xtor@gmail.com>
2018-05-24 04:55:04 -0300

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 //Book Struct (Model)
 type Book struct {
-	ID 		string `json:"id"`
+	ID    string `json:"id"`
 	Isbn  string `json:"isbn"`
 	Title string `json:"title"`
 	Author *Author `json:"author"` //this have it's own struct
